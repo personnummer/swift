219ebed31a9eeebed12c9afb98a8c3207dbd659c
Philip Fryklund <iosdev95@gmail.com>
2018-11-18 15:33:05 +0100

    Added documentation comments

--- source/Personnummer.swift
+++ source/Personnummer.swift
@@ -13,32 +13,82 @@ import Foundation
 
 
 
+/**
+	`Personnummer` model to validate and format a personnummer
+
+	Contains an initialized to get access to the components of a valid personnummer and convenient functions to just validate or reformat a personnummer.
+
+	# Usage
+	```
+	guard let p = Personummer(personnummer: inputPersonnummer) else {
+		fatalError("The personnummer input was incorrect")
+	}
+	```
+
+	### Or
+
+	```
+	if !Personnummer.isValid(inputPersonnummer) {
+		fatalError("The personnummer input was incorrect")
+	}
+	```
+*/
 public struct Personnummer {
 	
+	/**
+		Convenient function to check if a personnummer is valid
+	
+		- Parameter personnummer: The personnummer to be validated.
+		- Returns: Returns `true` if `personnummer` is valid and `false` if invalid.
+	*/
 	public static func isValid(_ personnummer: String) -> Bool {
 		return Personnummer(personnummer: personnummer) != nil
 	}
 	
+	/**
+		Convenience method to format a valid personnummer
+	
+		- Parameter personnummer: The personnummer to be formatted.
+		- Parameter longFormat: Long format is `yyyymmddnnnn`, short format is `yymmdd(+ or -)nnnn`.
+		- Returns: Returns the formatted personnummer if `personnummer` was valid, otherwise returns `nil`.
+	*/
 	public static func format(_ personnummer: String, longFormat: Bool = false) -> String? {
 		return Personnummer(personnummer: personnummer)?.format(longFormat: longFormat)
 	}
 	
-		
+	
+	/// Separator between birth date and last four digits
 	public enum Separator: Character {
+		/// Person is younger than 100 years old
 		case youngerThan100 = "-"
+		/// Person is older than 100 years old
 		case olderThan100 = "+"
 	}
 	
+	/// The string used to initialize `Self`.
 	public let inputPersonnummer: String
+	/// The birth century Example: 1995, century = 19.
 	public let century: Int
+	/// The birth year of the century Example: 1995, year = 95.
 	public let year: Int
+	/// The birth month Example: 9.
 	public let month: Int
+	/// The birth day Example: 12.
 	public let day: Int
+	/// The separator indicating wether the person is older than 100 or not (+ or -).
 	public let separator: Separator
+	/// The last four digits.
 	public let fourLast: Int
 	
 	
 	
+	/**
+		Method to format a valid personnummer
+	
+		- Parameter personnummer: The personnummer to be formatted.
+		- Parameter longFormat: Long format is `yyyymmddnnnn`, short format is `yymmdd(+ or -)nnnn`.
+		- Returns: Returns the formatted personnummer.
+	*/
 	public func format(longFormat: Bool = false) -> String {
 		if longFormat {
 			return "\(String(format: "%02i", century))\(String(format: "%02i", year))\(String(format: "%02i", month))\(String(format: "%02i", day))\(String(format: "%04i", fourLast))"
@@ -49,7 +99,14 @@ public struct Personnummer {
 	}
 	
 	
+	/**
+		Try to initialize a new `Personnummer` model.
 	
+		Checks if `personnummer` is correctly formatted, is an actual date, conforms to Luhn Algorithm.
+	
+		- Parameter personnummer: The personnummer to be used to initialize.
+		- Returns: Returns ´Personnummer´ if `personnummer` is valid and ´nil´ if invalid.
+	*/
 	public init?(personnummer: String) {
 		let regexPattern = "^(\\d{2}){0,1}(\\d{2})(\\d{2})(\\d{2})([\\+\\-\\s]?)(\\d{4})$"
 		let regexer = try! NSRegularExpression.init(pattern: regexPattern, options: [.anchorsMatchLines])
